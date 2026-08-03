ce4b659a16a7622068dc504b5afae00fc75f7e0b
Levente Blanar <blanar.levente@gmail.com>
2026-08-03 15:25:12 +0200

    if

---
+++ BroCodeCourse/chapters/10-ifstatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace if_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+            if (age > 100)
+            {
+                Console.WriteLine("You are too old to sign up!");
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You are now signed up!");
+            }
+            else if (age < 0)
+            {
+                Console.WriteLine("You haven't been born yet!");
+            }
+            else
+            {
+                Console.WriteLine("you must be 18+ to sign up");
+            }
+        }
+    }
+}
