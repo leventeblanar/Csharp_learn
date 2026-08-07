aadf636b70c2ba2a39ba22095093bf0ce6fae155
Levente Blanar <blanar.levente@gmail.com>
2026-08-07 08:11:39 +0200

    dbCliens

--- BroCodeCourse/chapters/09-stringmethods/obj/Debug/net10.0/strings.AssemblyInfo.cs
+++ BroCodeCourse/chapters/09-stringmethods/obj/Debug/net10.0/strings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("strings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243998c39631f1e8f0897c1627fb20754c6958c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5057354c6a33f1cbb620bfd996055f26403d8341")]
 [assembly: System.Reflection.AssemblyProductAttribute("strings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("strings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
