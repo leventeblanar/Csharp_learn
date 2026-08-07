d20cd70340486dd88ea69365229c933061ca67bb
Levente Blanar <blanar.levente@gmail.com>
2026-08-07 10:18:32 +0200

    15-16

---
+++ BroCodeCourse/chapters/15_NestedLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿// nested loops = loops inside of other loops
+
+Console.WriteLine("How many rows? ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many columns? ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What symbol? ");
+String symbol = Console.ReadLine();
+
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(symbol);
+    }
+    Console.WriteLine();
+}
