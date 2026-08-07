1b8ce56dc87dbd450d1839b306f2df7976a76ec0
Levente Blanar <blanar.levente@gmail.com>
2026-08-07 14:07:46 +0200

    18-19

---
+++ BroCodeCourse/chapters/18_arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿// array = a variable that can store multiple values. fixed size
+
+// String[] cars = {"BMW", "Mustang", "Corvette"};
+
+String[] cars = new string[3];  // declare array and then assign values
+
+cars[0] = "Tesla";
+cars[1] = "Mustang";
+cars[2] = "Corvette";
+// cars[3] = "Audi"; // this won't work -> index outsude the bounds fo the array
+
+Console.WriteLine(cars[0]);
+Console.WriteLine(cars[1]);
+Console.WriteLine(cars[2]);
+// Console.WriteLine(cars[3]);
+
+for (int i = 0; i < cars.Length; i++)
+{
+    Console.WriteLine(cars[i]);
+}
---
+++ BroCodeCourse/chapters/19_foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿// array = a variable that can store multiple values. fixed size
+
+String[] cars = {"BMW", "Mustang", "Corvette"};
+
+Console.WriteLine(cars[0]);
+Console.WriteLine(cars[1]);
+Console.WriteLine(cars[2]);
+// Console.WriteLine(cars[3]);
+
+for (int i = 0; i < cars.Length; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+
+foreach (String car in cars)
+{
+    Console.WriteLine(car);
+}
